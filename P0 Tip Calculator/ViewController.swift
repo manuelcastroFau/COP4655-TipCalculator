ed1628b6260b8e0dc032cc6e62bf9f2caec6ac5b
Manuel Castro <mhcd1984@gmail.com>
2022-08-20 23:46:26 -0400

    tip calculator

--- P0 Tip Calculator/ViewController.swift
+++ P0 Tip Calculator/ViewController.swift
@@ -9,6 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var percentageLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    
+    @IBAction func calcTipSlider(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let percent = (floor(Double(tipSlider.value)) + 15)/100.0
+        let tip = bill * percent
+        let total = tip + bill
+        
+        percentageLabel.text = String( format: "%.0F",(percent*100))+" %"
+        tipAmountLabel.text = String(format: "$%.2F" ,tip)
+        totalLabel.text = String(format: "$%.2F" ,total)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentage  = [0.15, 0.18, 0.20]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipAmountLabel.text = String(format: "$%.2F" ,tip)
+        totalLabel.text = String(format: "$%.2F" ,total)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
