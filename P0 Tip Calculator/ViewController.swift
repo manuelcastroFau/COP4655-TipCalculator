f010be222f7d5c9d55dc3cd3fdacf39c11ba8d91
Manuel Castro <mhcd1984@gmail.com>
2022-08-20 22:24:42 -0400

    Initial Commit

---
+++ P0 Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P0 Tip Calculator
+//
+//  Created by manuel  castro  on 8/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
